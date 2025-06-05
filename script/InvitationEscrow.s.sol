9f1e79203ba1c90a99a5237ce3d29165dff90729
roleengineer <roleengineer@gmail.com>
2025-06-05 16:47:57 +0200

    (deployment): deployed to gnosis

--- script/InvitationEscrow.s.sol
+++ script/InvitationEscrow.s.sol
@@ -5,12 +5,13 @@ import {Script, console} from "forge-std/Script.sol";
 import {InvitationEscrow} from "src/InvitationEscrow.sol";
 
 contract InvitationEscrowScript is Script {
-    InvitationEscrow public invitationEscrow;
+    address deployer = address(0xb1B1A7BDf196d1eB45a652E948971C3E790C4Ae2);
+    InvitationEscrow public invitationEscrow; // 0x3167D6c3F718A8CD68788538F39eD75f2A79C453
 
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        vm.startBroadcast(deployer);
 
         invitationEscrow = new InvitationEscrow();
 
