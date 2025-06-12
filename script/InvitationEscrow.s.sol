f151f033a188be1557addd859119ae04c2312c5d
roleengineer <roleengineer@gmail.com>
2025-06-12 12:26:39 +0200

    (deployment): audited deployment gnosis chain

--- script/InvitationEscrow.s.sol
+++ script/InvitationEscrow.s.sol
@@ -6,7 +6,7 @@ import {InvitationEscrow} from "src/InvitationEscrow.sol";
 
 contract InvitationEscrowScript is Script {
     address deployer = address(0xb1B1A7BDf196d1eB45a652E948971C3E790C4Ae2);
-    InvitationEscrow public invitationEscrow; // 0x3167D6c3F718A8CD68788538F39eD75f2A79C453
+    InvitationEscrow public invitationEscrow; // 0x0956C08aD2DcC6f4a1e0Cc5Ffa3a08D2A6D85F29
 
     function setUp() public {}
 
